26087d0c9ab376d9bcc3030e5f7f1c308d8e96e4
pooya haratian <pooyaharatian@gmail.com>
2019-01-03 18:26:01 +0330

    first

---
+++ src/config/express.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const morgan = require('morgan');
+const helmet = require('helmet');
+const routes = require('./routes');
+const { logs, env } = require('./vars');
+const error = require('./error');
+
+
+/**
+* Express instance
+* @public
+*/
+const app = express();
+
+// *** SO IMPORTANT *** this will handle promises on routers. 
+require('express-async-errors');
+
+// request logging. dev: console | production: file
+app.use(morgan(logs));
+
+// secure apps by setting various HTTP headers
+app.use(helmet());
+
+// routes
+app.use(routes);
+
+if (env !== 'development') {
+    app.use(error)
+}
+
+module.exports = app;
